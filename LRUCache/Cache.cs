79a21600ad2c1726ea03213073061e40c9268352
mikah <mikah@mltd.com>
2020-10-08 15:35:25 -0700

    Finished LRUCache

--- LRUCache/Cache.cs
+++ LRUCache/Cache.cs
@@ -1,27 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace LRUCache
 {
     class Cache<TKey, TValue> : ICache<TKey, TValue>
     {
-        public Dictionary<TKey, LinkedListNode<(TValue Value, TKey Key)>> dictionary;
-        public LinkedList<(TValue Value, TKey Key)> list;
+        public Dictionary<TKey, LinkedListNode<KeyValuePair<TKey, TValue>>> dictionary;
+        public LinkedList<KeyValuePair<TKey, TValue>> list;
         public int maxCount {get; set;}
-        public Cache()
+        public Cache(int count)
         {
-            dictionary = new Dictionary<TKey, LinkedListNode<(TValue Value, TKey Key)>>();
-            list = new LinkedList<(TValue Value, TKey Key)>();
+            dictionary = new Dictionary<TKey, LinkedListNode<KeyValuePair<TKey, TValue>>>();
+            list = new LinkedList<KeyValuePair<TKey, TValue>>();
+            maxCount = count;
         }
 
         public bool TryGetValue(TKey key, out TValue value)
         {
             if(dictionary.ContainsKey(key))
             {
-                value = dictionary[key].Value;
-                list.Remove((value, key));
-                list.AddFirst((value, key));
+                value = dictionary[key].Value.Value;
+                list.Remove(new KeyValuePair<TKey, TValue>(key, value));
+                list.AddFirst(new KeyValuePair<TKey, TValue>(key, value));
                 return true;
             }
             value = default;
@@ -30,18 +32,24 @@ namespace LRUCache
 
         public void Put(TKey key, TValue value)
         {
-            if(dictionary.ContainsKey(key))
+            if (dictionary.ContainsKey(key))
             {
-                dictionary[key].Value = value;
+                list.Remove(list.Find(new KeyValuePair<TKey, TValue>(key, dictionary[key].Value.Value)));
+                dictionary[key].Value = new KeyValuePair<TKey, TValue>(key, value);
+                list.AddFirst(new KeyValuePair<TKey, TValue>(key, value));
             }
-            list.AddFirst((value, key));
-            dictionary.Add(key, list.First);
-            
+            else
+            {
+                list.AddFirst(new KeyValuePair<TKey, TValue>(key, value));
+                dictionary.Add(key, list.First);
+            }
+
             if(list.Count > maxCount)
             {
-                dictionary.Remove();
-                list.RemoveLast();
+                dictionary.Remove(list.Last.Value.Key);
+                list.Remove(list.Last);
             }
+
         }
 
     }
--- LRUCache/Program.cs
+++ LRUCache/Program.cs
@@ -6,7 +6,30 @@ namespace LRUCache
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Cache<int, char> cachhe = new Cache<int, char>(5);
+
+            cachhe.Put(1, 'a');
+            cachhe.Put(2, 'b');
+            cachhe.Put(3, 'c');
+            cachhe.Put(4, 'd');
+            cachhe.Put(5, 'e');
+            cachhe.Put(6, 'f');
+
+            char a = default;
+            bool shouldntExist = cachhe.TryGetValue(1, out a);
+
+            char b = default;
+            bool shouldExist = cachhe.TryGetValue(2, out b);
+
+            cachhe.Put(7, 'g');
+
+            char c = default;
+            bool shouldntTrue = cachhe.TryGetValue(1, out c);
+
+            char bb = default;
+            bool shouldTrue = cachhe.TryGetValue(2, out bb);
+
         }
     }
 }
